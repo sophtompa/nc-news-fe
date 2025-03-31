3d069d2943d221eda602f0beb16a9cc8e764535d
Sophie <sophtompa@gmail.com>
2025-03-31 15:43:22 +0100

    add Header, Nav, Welcome components

---
+++ components/Nav.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router"
+
+function Nav() {
+
+    return (
+        <nav>
+            <h3>Nav</h3>
+            {/* <Link className='link' to='/'>Home</Link>
+            <Link className='link' to='/articles'>Articles</Link> */}
+        </nav>
+    )
+}
+
+export default Nav;
---
+++ components/Welcome.jsx
@@ -0,0 +1,11 @@
+function Welcome() {
+
+    return (
+        <section>
+            <h2>Welcome!</h2>
+            <button>Start Browsing</button>
+        </section>
+    )
+}
+
+export default Welcome;

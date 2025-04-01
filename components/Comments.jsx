925eb66f0f9574b8da874f8e3b891111d6206de1
Sophie <sophtompa@gmail.com>
2025-04-01 15:29:48 +0100

    add comment component

---
+++ components/Comments.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router"
+import { getComments } from "../api"
+
+function Comments() {
+    const { article_id } = useParams();
+    const [isLoading, setLoading] = useState(true)
+    const [comments, setComments] = useState([])
+    const [isVisible, setIsVisible] = useState(true)
+
+    useEffect(
+        () => { 
+            getComments(article_id).then((commentsData) => { 
+                setComments(commentsData)
+                setLoading(false)
+            })
+        }, [article_id])
+
+
+    if (isLoading) return <p>Loading...</p>;
+    if (!comments) return <p>No comments yet.</p>
+
+    
+
+    return (
+        <>
+        <h1>Comments</h1> 
+        <button className='hide-show' onClick={() => setIsVisible(!isVisible)}>{isVisible ? 'Hide comments' : 'Show comments'}</button>
+        {isVisible && (
+            <div>
+        {comments.map((comment) => {
+            return (
+                <ul key={comment.comment_id}>
+                <li>{comment.body}</li>
+                <li>Posted by: {comment.author}</li>
+                <li>Posted at: {comment.created_at}</li>
+                <li>Votes: {comment.votes}</li>
+                </ul>
+            )
+        })}
+            </div>
+        )}
+       
+        </>
+    )
+}
+
+export default Comments;
